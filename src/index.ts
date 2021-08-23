f0363a971b1cd29daada6082fbc1b135bf593f82
Nick McBurney <nick.mcburney@ocean.co.uk>
2021-08-23 19:07:17 +0100

    Package name change and default export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ if (module && module.hot && module.hot.decline) {
   module.hot.decline();
 }
 
+import vueRouter from './withVueRouter'
+
 // make it work with --isolatedModules
-export default {};
+export default vueRouter;
