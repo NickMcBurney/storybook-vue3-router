1547d24804ab8597cad6d86130a039da0adb2a67
Nick McBurney <nick@nickmcburney.co.uk>
2021-08-28 11:56:35 +0100

    Fix to ensure vue router only initialised once

--- src/withVueRouter.ts
+++ src/withVueRouter.ts
@@ -10,32 +10,38 @@ export const withVueRouter = (routesParam?: Array<RouteRecordRaw>, beforeEach?: 
     parameterName: 'withVueRouter',
 
     wrapper: (storyFn, context) => {
-      /* get routes from either `routesParam` or use `defaultRoutes` */
-      const routes: Array<RouteRecordRaw> = routesParam || defaultRoutes
+      const vueRouterGlobalProperty = app.config.globalProperties.$router
+      const vueRouteGlobalProperty = app.config.globalProperties.$route
+      if (context.viewMode === 'story' && !vueRouterGlobalProperty && !vueRouteGlobalProperty) {
+        console.log(app)
+        /* get routes from either `routesParam` or use `defaultRoutes` */
+        const routes: Array<RouteRecordRaw> = routesParam || defaultRoutes
 
-      /* create vue router */
-      const router = createRouter({
-        history: createWebHashHistory(),
-        routes
-      });
+        /* create vue router */
+        const router = createRouter({
+          history: createWebHashHistory(),
+          routes
+        });
 
-      /* check if there is a `beforeEach` function passed into decorator function */
-      if (typeof beforeEach === 'function') {
-        /* fire `beforeEach` param on `router.beforeEach` and pass `to` and `from` params to the function */
-        router.beforeEach((to, from) => beforeEach(to, from))
-      }
+        /* check if there is a `beforeEach` function passed into decorator function */
+        if (typeof beforeEach === 'function') {
+          /* fire `beforeEach` param on `router.beforeEach` and pass `to` and `from` params to the function */
+          router.beforeEach((to, from) => beforeEach(to, from))
+        }
 
-      /* tell storybook to use vue router */
-      app.use(router)
+        /* tell storybook to use vue router */
+        app.use(router)
 
-      /* if the route route is not `/` */
-      if (routes[0].path !== '/') {
-        /* update router to use the first path in routes defined */
-        router.replace(routes[0].path)
+        /* if the route route is not `/` */
+        if (routes[0].path !== '/') {
+          /* update router to use the first path in routes defined */
+          router.replace(routes[0].path)
+        }
       }
       
       /* return the storybook story */
       return storyFn(context);
+    
     }
   })
 
