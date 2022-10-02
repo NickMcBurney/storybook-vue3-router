2337c7324db5d6e933a73435d6841d264dfa7e0e
Nick McBurney <nick@nickmcburney.co.uk>
2022-10-02 13:39:11 +0100

    Update test workflow and basic playwright test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Loads Inital README page', async ({ page }) => {
+  await page.goto(process.env.PLAYWRIGHT_TEST_BASE_URL as string);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/README/);
 
-  // create a locator
-  const getStarted = page.locator('text=Get Started');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await expect(page.locator('h1')).toHaveText('Storybook Vue3 Router');
 });
