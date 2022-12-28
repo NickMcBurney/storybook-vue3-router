0bb4e857439bee51b4a2eea91d170bc215ad00ab
Nick McBurney <nick@nickmcburney.co.uk>
2022-12-28 15:43:24 +0000

    storybook 6.5 > 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-  stories: [
-    "../**/**/*.stories.mdx",
-    "../**/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../**/**/*.stories.mdx", "../**/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["../preset.js", "@storybook/addon-essentials"],
+  docs: {
+    autodocs: true
+  },
+  framework: {
+    name: "@storybook/vue3-webpack5",
+    options: {}
+  }
 };
