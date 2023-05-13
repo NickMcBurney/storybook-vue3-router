d8340a5eeea24209b9c61590bccdbf18e9770585
Nick McBurney <nick@nickmcburney.co.uk>
2023-05-13 07:16:13 +0100

    README.spec typo fix

--- tests/README.spec.ts
+++ tests/README.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 const PLAYWRIGHT_TEST_BASE_URL = process.env.PLAYWRIGHT_TEST_BASE_URL as string || "https://storybook-vue3-router.netlify.app/"
 
-test('Loads Inital README page', async ({ page }) => {
+test('Loads Initial README page', async ({ page }) => {
   await page.goto(PLAYWRIGHT_TEST_BASE_URL);
 
   // Expect a title "to contain" a substring.
