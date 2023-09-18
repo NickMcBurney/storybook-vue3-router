d79ae21f3d38f4e4e17c5739e0733d1831647ffa
Nick McBurney <nick@nickmcburney.co.uk>
2023-09-18 16:00:02 +0100

    Adding global button component

---
+++ examples/components/BasicButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const disabled = ref(false)
+
+function click() {
+  disabled.value = true
+}
+</script>
+
+<template>
+  <button @click="click" :disabled="disabled"><slot /></button>
+</template>
