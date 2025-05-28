02266474bc2956c0f9cda2f8338c59e0b1ccf9d5
Ben Sheedy <ben.sheedy@gosuperscript.com>
2025-05-28 22:56:26 +0100

    fix: mdx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-  stories: ["../examples/**/*.stories.mdx", "../examples/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["./../examples/**/*.mdx", "../examples/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["../preset.js", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/vue3-vite",
-    options: {}
-  }
+    options: {},
+  },
 };
